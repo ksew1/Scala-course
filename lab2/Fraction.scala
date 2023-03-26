b73eb65417d1525d4efef3175145d44df7b9f606
Karol Sewiło <95349104+ksew1@users.noreply.github.com>
2023-03-26 20:20:07 +0200

    lab2

--- lab2/Fraction.scala
+++ lab2/Fraction.scala
@@ -1,32 +1,66 @@
 trait Fraction {
-  val num: Int
-  val denom: Int
+  val numerator: Int
+  val denominator: Int
 
   def *(other: Fraction): Fraction
+
+  def -(other: Fraction): Fraction
+
+  def +(other: Fraction): Fraction
+
+  def /(other: Fraction): Fraction
 }
 
 
 object Fraction {
   // one of the "creational patterns/idioms"
-  def apply(num: Int, denom: Int, loggable: Boolean = false): Fraction =
-    if (loggable) new LoggableImpl(num, denom)
-    else new DefaultImpl(num, denom)
+  def apply(num: Int, denominator: Int, loggable: Boolean = false): Fraction =
+    if (loggable) new LoggableImpl(num, denominator)
+    else new DefaultImpl(num, denominator)
 
-  private class DefaultImpl(val num: Int, val denom: Int) extends Fraction {
+  private class DefaultImpl(val numerator: Int, val denominator: Int) extends Fraction {
     override def *(other: Fraction): Fraction =
-      Fraction(this.num * other.num, this.denom * other.denom)
+      Fraction(this.numerator * other.numerator, this.denominator * other.denominator)
 
-    override def toString: String = num.toString + "/" + denom.toString
+    override def -(other: Fraction): Fraction =
+      Fraction(this.numerator * other.denominator - other.numerator * this.denominator,
+        this.denominator * other.denominator)
+
+    override def +(other: Fraction): Fraction =
+      Fraction(this.numerator * other.denominator + other.numerator * this.denominator,
+        this.denominator * other.denominator)
+
+    override def /(other: Fraction): Fraction =
+      Fraction(this.numerator * other.denominator, this.denominator * other.numerator)
+
+    override def toString: String = numerator.toString + "/" + denominator.toString
   }
 
-  private class LoggableImpl(num: Int, denom: Int)
-    extends DefaultImpl(num, denom) with Loggable {
+  private class LoggableImpl(num: Int, denominator: Int)
+    extends DefaultImpl(num, denominator) with Loggable {
     private def timeStamp = System.nanoTime // to keep the example short...
 
     override def *(other: Fraction): Fraction = {
       log(timeStamp, "multiplying " + this.toString + " by " + other.toString)
       // super.*(other) is not loggable
-      Fraction(this.num * other.num, this.denom * other.denom, loggable = true)
+      Fraction(this.num * other.numerator, this.denominator * other.denominator, loggable = true)
+    }
+
+    override def -(other: Fraction): Fraction = {
+      log(timeStamp, "subtracting " + other.toString + " from " + this.toString)
+      Fraction(this.num * other.denominator - other.numerator * this.denominator,
+        this.denominator * other.denominator, loggable = true)
+    }
+
+    override def +(other: Fraction): Fraction = {
+      log(timeStamp, "adding " + other.toString + " to " + this.toString)
+      Fraction(this.num * other.denominator + other.numerator * this.denominator,
+        this.denominator * other.denominator, loggable = true)
+    }
+
+    override def /(other: Fraction): Fraction = {
+      log(timeStamp, "dividing " + this.toString + " by " + other.toString)
+      Fraction(this.num * other.denominator, this.denominator * other.numerator, loggable = true)
     }
   }
 }
